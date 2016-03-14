e2b51c3866db554159c9511df7daa1d0afcfa524
vagrant <prasad.vedurumudi@gmail.com>
2016-03-14 14:50:14 +0530

    Test

---
+++ mesostest/src/main/java/com/oracle/pcs/mesostest/TestMesos.java
@@ -0,0 +1,8 @@
+package com.oracle.pcs.mesostest;
+
+public class TestMesos {
+	public static void main (String [] args) {
+		System.out.println("This is a test program to test Mesos Jenkins plugin");
+	}
+
+}
